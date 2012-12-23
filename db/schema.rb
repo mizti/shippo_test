8a127d6a4d979b1b0c2653ca58994f10274b1bbe
mizti <mizugokoro.redirect+github@gmail.com>
2012-12-24 00:30:52 +0900

    added fixtures to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223110500) do
+ActiveRecord::Schema.define(:version => 20121223114632) do
+
+  create_table "books", :force => true do |t|
+    t.string   "isbn"
+    t.string   "title"
+    t.integer  "price"
+    t.string   "publish"
+    t.date     "published"
+    t.boolean  "cd"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pictures", :force => true do |t|
     t.integer  "user_id"
